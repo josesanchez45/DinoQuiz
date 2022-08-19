e2244749277c4dc7e3bd389a210d35b20295fbbf
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-18 17:30:49 -0700

    initial commit

---
+++ DinosaurQuiz/SimpleDinoQuiz.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DinosaurQuiz
+{
+    partial class SimpleDinoQuiz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DinosaurQuiz.Properties.Resources.dime_jpg;
+            this.pictureBox1.Location = new System.Drawing.Point(122, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1285, 530);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(79, 565);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(1328, 111);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "question";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 651);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(514, 114);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(902, 665);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(505, 109);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 797);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(514, 108);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(893, 797);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(514, 107);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1657, 1010);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ DinosaurQuiz/SimpleDinoQuiz.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinosaurQuiz
+{
+    public partial class SimpleDinoQuiz : Form
+    {
+        // quiz game variables
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+        public SimpleDinoQuiz()
+        {
+            InitializeComponent();
+
+            AskQuestion(questionNumber);
+
+            totalQuestions = 5;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+            }
+            if (questionNumber == totalQuestions)
+            {
+                //calculating percentage
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Quiz Complete!" + Environment.NewLine +
+                    "You have answered" + score + " questions correctly" + Environment.NewLine +
+                    "Your total percentage is " + percentage + "%" + Environment.NewLine +
+                    "Click Ok to play again"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                AskQuestion(questionNumber);
+            }
+
+            questionNumber++;
+            AskQuestion(questionNumber);
+
+        }
+        private void AskQuestion(int qNum)
+        {
+            switch (qNum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.dime_jpg;
+
+                    lblQuestion.Text = "This creature was not a dinosaur but still an ancient repltile. What was it called?";
+
+                    button1.Text = "Dimetrodon";
+                    button2.Text = "Sail o Saurus";
+                    button3.Text = "Spinosaurus";
+                    button4.Text = "Gallimimus";
+
+                    correctAnswer = 1;
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.raptor;
+
+                    lblQuestion.Text = "Famously misrepresented in films, what is this dinosaur called?";
+
+                    button1.Text = "This is a bird";
+                    button2.Text = "Kroxigor";
+                    button3.Text = "Postosuchus";
+                    button4.Text = "Velociraptor";
+
+                    correctAnswer = 4;
+
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.spino;
+
+                    lblQuestion.Text = "Thought to have mostly lurked in the water, what animal is this?";
+
+                    button1.Text = "Slann";
+                    button2.Text = "Dread Saurian";
+                    button3.Text = "Spinosaurus";
+                    button4.Text = "Nightmare Fuel";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.sue;
+
+                    lblQuestion.Text = "This is arguably the most famous Dinosaur ever, what was that dinos name?";
+
+                    button1.Text = "Sue";
+                    button2.Text = "T Rex";
+                    button3.Text = "Allosaurus";
+                    button4.Text = "Stegosaurus";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.trike;
+
+                    lblQuestion.Text = "In the Land Before Time she was know as Sara";
+
+                    button1.Text = "Pteredon";
+                    button2.Text = "Stegosaurus";
+                    button3.Text = "Triceratops";
+                    button4.Text = "Velociraptor";
+
+                    correctAnswer = 3;
+
+                    break;
+
+
+
+
+            }
+
+        }
+    }
+}
